a0580ac387c900cb3f932ad88381f32aa358435d
RayyaneElmalhouni <elmalhounirayyane@gmail.com>
2022-02-28 20:38:57 +0100

    Commiting html

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,24 @@
 <script>
+    import Timmer from './components/Timmer.svelte';
+    import HT from './components/HT.svelte'
 </script>
 
-<h1>Test</h1>	
-
 <style>
-	
+	h1, h3 {
+        text-align: center;
+    }
 </style>
+
+<h1>Handwashing app</h1>
+<Timmer />
+<HT />
+
+
+<h3>
+    <a href="https://phptuts.github.io/fcc-handwashing/handwashing.gif">
+        Picture Source
+    </a>
+    <a href="https://freesound.org/people/metrostock99/sounds/345086/">
+        Sound Source
+    </a>
+</h3>
---
+++ src/components/Progress.svelte
@@ -0,0 +1,18 @@
+<style>
+    .progress-container {
+        margin: 20px 0;
+        border: 1px solid rgb(194, 194, 194);
+    }
+    .progress-bar {
+        height: 30px;
+        background-color: rgb(77, 199, 55);
+    }
+</style>
+
+<div bp="grid">
+    <div bp="offset-5@md 4@md 12sm" class="progress-container">
+        <div class="progress-bar" style="width: 20%;">
+            <span class="sr-only"></span>
+        </div>
+    </div>
+</div>
